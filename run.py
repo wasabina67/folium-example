27f5fb71422ad833b43168e62197cc0d231802ac
wasabina67 <wasabina67@gmail.com>
2024-07-05 22:03:56 +0900

    Update run.py

--- run.py
+++ run.py
@@ -9,10 +9,21 @@ def main():
         "latitude": [],
         "longitude": [],
     }
-    _ = pd.DataFrame(data)
+    df = pd.DataFrame(data)
 
     base_location = [43.06417, 141.34694]
-    _ = folium.Map(location=base_location, zoom_start=8)
+    map = folium.Map(location=base_location, zoom_start=8)
+
+    for _, row in df.iterrows():
+        folium.CircleMarker(
+            location=[row["latitude"], row["longitude"]],
+            radius=10,
+            color="blue",
+            fill=True,
+            fill_color="blue",
+        ).add_to(map)
+
+    map.save("docs/index.html")
 
 
 if __name__ == "__main__":
