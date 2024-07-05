a4d903f055e03284078afc230c593860495063bf
wasabina67 <wasabina67@gmail.com>
2024-07-05 21:56:11 +0900

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,17 @@
+import folium
+import pandas as pd
+
+
 def main():
-    pass
+    data = {
+        "place": [],
+        "population": [],
+        "latitude": [],
+        "longitude": [],
+    }
+    _ = pd.DataFrame(data)
+
+    _ = folium.Map(location=[43.06417, 141.34694], zoom_start=8)
 
 
 if __name__ == "__main__":
